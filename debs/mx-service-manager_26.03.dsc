fff8b4e0a81dd65494a9d3a57de918d04c4de14f
Adrian <adrian@mxlinux.org>
2026-03-19 14:56:53 -0400

    New build

---
+++ debs/mx-service-manager_26.03.dsc
@@ -0,0 +1,33 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA512
+
+Format: 3.0 (native)
+Source: mx-service-manager
+Binary: mx-service-manager
+Architecture: any
+Version: 26.03
+Maintainer: Adrian <adrian@mxlinux.org>
+Homepage: https://github.com/MX-Linux/mx-service-manager
+Standards-Version: 3.9.8
+Vcs-Git: git://github.com/MX-Linux/mx-service-manager
+Build-Depends: debhelper-compat (= 13), cmake (>= 3.16), ninja-build, qt6-base-dev, qt6-base-dev-tools, qt6-tools-dev, qt6-tools-dev-tools
+Package-List:
+ mx-service-manager deb admin optional arch=any
+Checksums-Sha1:
+ 219542c615e4c1d11cce6c5f91776e4326f56c43 124684 mx-service-manager_26.03.tar.xz
+Checksums-Sha256:
+ e190e6dbb5e7e3cbe041a0de0602ab6f6fe1a3aaf84fc6595818bfd9681d1446 124684 mx-service-manager_26.03.tar.xz
+Files:
+ 3aaec06c69e5c72e0eceb07f8e5e01a9 124684 mx-service-manager_26.03.tar.xz
+
+-----BEGIN PGP SIGNATURE-----
+
+iQFHBAEBCgAxFiEE8ndToY6S45N+YzXncJOMeAZ57pgFAmm8RqYTHGFkcmlhbkBt
+eGxpbnV4Lm9yZwAKCRBwk4x4BnnumA49B/48O3U4qExlGyTTrXdByrTX26gS8llN
+CHgRzjpbamB33uoz1HsIv7aKAQq/acy+G5CCOGHFehLVcJEag3K8MkQksAfqyNpS
+56UPLNTqnF1Rf6xj/NVCK6cCyBCZ3BuyqUh8vAHak5AGOqeLx9yJSpiJ3lYcNkWh
+cKkwD35k4dxOGmzsXmB3prqCzie0ut5hsIHM8XodNIwW72qcLfqSbaKpEbhlAgjp
+NFvz8m/4WzIar54dpMYOjic29709ZpfAlZ182o2ntDDQokSN/TpalMYmGzO1q/in
+z6EH0wrjzPOPYIFMBjAE2LMnj/vO/W96EeoGhrapr2uuTyVKjiTCqEeZ
+=sHI1
+-----END PGP SIGNATURE-----
--- translations/mx-service-manager_en_US.ts
+++ translations/mx-service-manager_en_US.ts
@@ -1,15 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="it">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="en_US">
 <context>
     <name>Cmd</name>
     <message>
         <location filename="../src/cmd.cpp" line="71"/>
         <source>Administrator Access Required</source>
-        <translation type="unfinished"/>
+        <translation>Administrator Access Required</translation>
     </message>
     <message>
         <location filename="../src/cmd.cpp" line="72"/>
         <source>This operation requires administrator privileges. Please restart the application and enter your password when prompted.</source>
-        <translation type="unfinished"/>
+        <translation>This operation requires administrator privileges. Please restart the application and enter your password when prompted.</translation>
     </message>
 </context>
 <context>
@@ -18,24 +18,24 @@
         <location filename="../src/mainwindow.ui" line="14"/>
         <location filename="../src/mainwindow.cpp" line="607"/>
         <source>MX Service Manager</source>
-        <translation>MX Gestore servizi</translation>
+        <translation>MX Service Manager</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="26"/>
         <location filename="../src/mainwindow.cpp" line="191"/>
         <location filename="../src/mainwindow.cpp" line="623"/>
         <source>&amp;Enable at boot</source>
-        <translation>&amp;Abilita all&apos;avvio</translation>
+        <translation>&amp;Enable at boot</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="62"/>
         <source>About this application</source>
-        <translation>Informazioni su questa applicazione</translation>
+        <translation>About this application</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="65"/>
         <source>&amp;About...</source>
-        <translation>&amp;Informazioni...</translation>
+        <translation>&amp;About...</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="71"/>
@@ -45,12 +45,12 @@
     <message>
         <location filename="../src/mainwindow.ui" line="125"/>
         <source>Display help </source>
-        <translation>Visualizza la guida</translation>
+        <translation>Display help </translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="128"/>
         <source>&amp;Help</source>
-        <translation>A&amp;iuto</translation>
+        <translation>&amp;Help</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="134"/>
@@ -60,12 +60,12 @@
     <message>
         <location filename="../src/mainwindow.ui" line="166"/>
         <source>Quit application</source>
-        <translation> Chiudi applicazione</translation>
+        <translation>Quit application</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="169"/>
         <source>&amp;Close</source>
-        <translation>C&amp;hiudi</translation>
+        <translation>&amp;Close</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="175"/>
@@ -82,29 +82,29 @@
         <location filename="../src/mainwindow.ui" line="240"/>
         <location filename="../src/mainwindow.cpp" line="536"/>
         <source>Running services</source>
-        <translation>Servizi in esecuzione</translation>
+        <translation>Running services</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="245"/>
         <location filename="../src/mainwindow.cpp" line="537"/>
         <source>Services enabled at boot</source>
-        <translation>Servizi abilitati all&apos;avvio</translation>
+        <translation>Services enabled at boot</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="250"/>
         <location filename="../src/mainwindow.cpp" line="538"/>
         <source>Services disabled at boot</source>
-        <translation>Servizi disabilitati all&apos;avvio</translation>
+        <translation>Services disabled at boot</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="293"/>
         <source>&amp;Refresh services</source>
-        <translation type="unfinished"/>
+        <translation>&amp;Refresh services</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="306"/>
         <source>search</source>
-        <translation>cerca</translation>
+        <translation>search</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="258"/>
@@ -112,25 +112,25 @@
         <location filename="../src/mainwindow.cpp" line="87"/>
         <location filename="../src/mainwindow.cpp" line="91"/>
         <source>Loading...</source>
-        <translation>Caricamento...</translation>
+        <translation>Loading...</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="230"/>
         <location filename="../src/mainwindow.cpp" line="535"/>
         <location filename="../src/mainwindow.cpp" line="540"/>
         <source>System services</source>
-        <translation type="unfinished"/>
+        <translation>System services</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="235"/>
         <location filename="../src/mainwindow.cpp" line="539"/>
         <source>User services</source>
-        <translation type="unfinished"/>
+        <translation>User services</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="277"/>
         <source>This tool lists services and daemons (processes that run in the background) found on this system. Be careful to avoid stopping something that might disrupt a critical component; best to leave it alone if you don&apos;t know what it is.</source>
-        <translation>Questo strumento elenca i servizi e i demoni (processi eseguiti in background) presenti su questo sistema. Fai attenzione a evitare di fermare qualcosa che potrebbe interrompere un componente critico; meglio lasciarlo stare se non sai di cosa si tratti.</translation>
+        <translation>This tool lists services and daemons (processes that run in the background) found on this system. Be careful to avoid stopping something that might disrupt a critical component; best to leave it alone if you don&apos;t know what it is.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="73"/>
@@ -139,53 +139,53 @@
         <location filename="../src/mainwindow.cpp" line="680"/>
         <location filename="../src/mainwindow.cpp" line="689"/>
         <source>Error</source>
-        <translation>Errore</translation>
+        <translation>Current kernel doesn&apos;t support selected compression algorithm, please edit the configuration file and select a different algorithm.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="74"/>
         <source>Could not determine the init system. This program is supposed to run either with systemd or sysvinit</source>
-        <translation>Impossibile determinare il sistema di init. Questo programma dovrebbe funzionare sia con systemd che con sysvinit</translation>
+        <translation>Could not determine the init system. This program is supposed to run either with systemd or sysvinit</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="178"/>
         <location filename="../src/mainwindow.cpp" line="678"/>
         <source>S&amp;tart</source>
-        <translation>A&amp;vvio</translation>
+        <translation>S&amp;tart</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="184"/>
         <source>&amp;Disable at boot</source>
-        <translation>&amp;Disabilita all&apos;avvio</translation>
+        <translation>&amp;Disable at boot</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="579"/>
         <source>[User] </source>
-        <translation type="unfinished"/>
+        <translation>[User] </translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="587"/>
         <source>%1 total services, %2 currently &lt;font color=&apos;%3&apos;&gt;running&lt;/font&gt;</source>
-        <translation>%1 servizi totali, %2 attualmente &lt;font color=&apos;%3&apos;&gt;in esecuzione&lt;/font&gt;</translation>
+        <translation>%1 total services, %2 currently &lt;font color=&apos;%3&apos;&gt;running&lt;/font&gt;</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="592"/>
         <source>%1 &lt;font color=&apos;%2&apos;&gt;enabled&lt;/font&gt; at boot, but not running</source>
-        <translation>%1 &lt;font color=&apos;%2&apos;&gt;abilitato&lt;/font&gt; all&apos;avvio, ma non in esecuzione</translation>
+        <translation>%1 &lt;font color=&apos;%2&apos;&gt;enabled&lt;/font&gt; at boot, but not running</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="607"/>
         <source>About %1</source>
-        <translation>Circa %1</translation>
+        <translation>About %1</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="608"/>
         <source>Version: </source>
-        <translation>Versione: </translation>
+        <translation>Version: </translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="609"/>
         <source>Service and daemon manager</source>
-        <translation>Gestore di servizi e demoni</translation>
+        <translation>Service and daemon manager</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="611"/>
@@ -195,12 +195,12 @@
     <message>
         <location filename="../src/mainwindow.cpp" line="612"/>
         <source>%1 License</source>
-        <translation>%1 Licenza</translation>
+        <translation>%1 License</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="625"/>
         <source>Could not enable %1</source>
-        <translation>Impossibile abilitare %1</translation>
+        <translation>Could not enable %1</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="629"/>
@@ -208,53 +208,53 @@
         <location filename="../src/mainwindow.cpp" line="685"/>
         <location filename="../src/mainwindow.cpp" line="694"/>
         <source>Success</source>
-        <translation>Operazione riuscita</translation>
+        <translation>Success</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="629"/>
         <source>%1 was enabled at boot time.</source>
-        <translation>%1 è stato abilitato all&apos;avvio.</translation>
+        <translation>%1 was enabled at boot time.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="632"/>
         <source>Could not disable %1</source>
-        <translation>Impossibile disabilitare %1</translation>
+        <translation>Could not disable %1</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="636"/>
         <source>%1 was disabled.</source>
-        <translation>%1 è stato disabilitato.</translation>
+        <translation>%1 was disabled.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="644"/>
         <source>%1 Help</source>
-        <translation>%1 Aiuto</translation>
+        <translation>%1 Help</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="654"/>
         <location filename="../src/mainwindow.cpp" line="657"/>
         <source>Refreshing...</source>
-        <translation type="unfinished"/>
+        <translation>Refreshing...</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="680"/>
         <source>Could not start %1</source>
-        <translation>Impossibile iniziare %1</translation>
+        <translation>Could not start %1</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="685"/>
         <source>%1 was started.</source>
-        <translation>%1 è stato iniziato.</translation>
+        <translation>%1 was started.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="689"/>
         <source>Could not stop %1</source>
-        <translation>Impossibile fermare %1</translation>
+        <translation>Could not stop %1</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="694"/>
         <source>%1 was stopped.</source>
-        <translation>%1 è stato fermato.</translation>
+        <translation>%1 was stopped.</translation>
     </message>
 </context>
 <context>
@@ -262,45 +262,45 @@
     <message>
         <location filename="../src/about.cpp" line="71"/>
         <source>License</source>
-        <translation>Licenza</translation>
+        <translation>License</translation>
     </message>
     <message>
         <location filename="../src/about.cpp" line="72"/>
         <location filename="../src/about.cpp" line="82"/>
         <source>Changelog</source>
-        <translation>Registro delle modifiche</translation>
+        <translation>Changelog</translation>
     </message>
     <message>
         <location filename="../src/about.cpp" line="73"/>
         <source>Cancel</source>
-        <translation>Annulla</translation>
+        <translation>Cancel</translation>
     </message>
     <message>
         <location filename="../src/about.cpp" line="95"/>
         <source>&amp;Close</source>
-        <translation>&amp;Chiudi</translation>
+        <translation>&amp;Close</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="85"/>
         <location filename="../src/main.cpp" line="93"/>
         <source>Error</source>
-        <translation>Errore</translation>
+        <translation>Current kernel doesn&apos;t support selected compression algorithm, please edit the configuration file and select a different algorithm.</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="86"/>
         <source>You seem to be logged in as root, please log out and log in as normal user to use this program.</source>
-        <translation>Sembra che tu sia loggato come root, fai il log out e poi il log in come utente normale per usare questo programma.</translation>
+        <translation>You seem to be logged in as root, please log out and log in as normal user to use this program.</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="94"/>
         <source>You must run this program with admin access.</source>
-        <translation>Devi eseguire questo programma come amministratore.</translation>
+        <translation>You must run this program with admin access.</translation>
     </message>
     <message>
         <location filename="../src/service.cpp" line="162"/>
         <location filename="../src/service.cpp" line="164"/>
         <source>Could not find service description</source>
-        <translation>Impossibile trovare la descrizione del servizio</translation>
+        <translation>Could not find service description</translation>
     </message>
 </context>
 </TS>
